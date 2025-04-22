d7194927f6d4363f8e75c03c4a3da5289850c21e
Gina-4 <Georgina.Riri4@gmail.com>
2025-04-23 11:56:30 +1200

    Review redo

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+let log = document.getElementById('Log')
+let buttons = document.querySelectorAll('.buttons');
+let blue = document.getElementById('Blue');
+let white = document.getElementById('White');
+
+log.addEventListener('click', function () {
+})
+
+
+    const time = new Date();
+    const hours = date.getHours();
+    let minutes = date.getMinutes();
+ let minutesWithTwoDigits = minutes.toString().padStart(2, "O")
+        console.log(hours + ":" + minutesWithTwoDigits);
+
+
+
 document.getElementById("alert")
-addEventListener('click', function() {
-    this.alert("stop clicking the button")
+    .addEventListener('click', function () {
+        alert("stop clicking the button")
+    });
+
+
+blue.addEventListener('click', function () {
+    buttons.forEach(function (element) {
+        element.style.backgroundColor = "blue";
+    })
+});
+
+white.addEventListener('click', function () {
+    buttons.forEach(function (element) {
+        element.style.backgroundColor = "white";
+    })
 });
